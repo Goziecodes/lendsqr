33e5509ba3a3fbf5963f5d5e99d055d1d416bbfb
chigozie <ezechukwuchigozie@gmail.com>
2022-10-11 00:58:10 +0100

    switched server to typescript file

---
+++ server.ts
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+const authRoutes = require('./routes/auth.route');
+
+
+
+const customExpress = Object.create(express().response, {
+	data: {
+		value(data: any) {
+			return this.type('json').json({
+				status: 'success',
+				data,
+			});
+		},
+	},
+	error: {
+		value(error: any, message = 'An error occured') {
+			return this.json({
+				message,
+				status: 'error',
+				error,
+			});
+		},
+	},
+	errorMessage: {
+		value(message = 'API response message') {
+			return this.json({
+				message,
+				status: 'error',
+			});
+		},
+	},
+});
+
+app.use(express.json());
+app.use(express.urlencoded({
+	extended: false,
+}));
+
+app.use(authRoutes);
+
+app.response = Object.create(customExpress);
+
+
+module.exports = app;
