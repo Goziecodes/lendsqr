4725c97fdb90ab83983d51b6c4449004fba81f24
chigozie <ezechukwuchigozie@gmail.com>
2022-10-11 19:45:19 +0100

    added login validation schema and controller

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -13,10 +13,21 @@ export class AuthController {
 			password: joi.string().required()
 		});
 	}
+	static get loginSchema() {
+		return joi.object().keys({
+			email: joi.string().email().required(),
+			password: joi.string().required()
+		});
+	}
 
 	// eslint-disable-next-line no-unused-vars
 	static async register(req: Request, res: IExpressResponse | Response, next: NextFunction) {
 		const user = await authService.createUser(req.body as Partial<User>);
+		(res.status(201) as IExpressResponse).data(user);
+	}
+
+	static async login(req: Request, res: IExpressResponse | Response, next: NextFunction) {
+		const user = await authService.login(req.body as Pick<User, "email" | "password">);
 		(res.status(201) as IExpressResponse).data(user);
 	}
 };
