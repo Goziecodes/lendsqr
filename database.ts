c340fbc9707b38ff02e17add94d1359720754d88
chigozie <ezechukwuchigozie@gmail.com>
2022-10-11 06:38:35 +0100

    fixed bug where db instance exported was undefined

--- database.ts
+++ database.ts
@@ -1,19 +1,19 @@
 import databaseConfig from './knexfile'
 import Knex, { Knex as KnexType} from 'knex';
 
-// make null before connection is initialized
-export let database: KnexType<any, unknown[]>;
+// database singleton
 const env = (process.env.NODE_ENV || 'development') as keyof typeof databaseConfig;
+const database: KnexType<any, unknown[]> = Knex(databaseConfig[env]); 
 
 export const connectDatabase =  async () => {
   try {
-     database = Knex(databaseConfig[env]);
      await database.raw('SELECT 1');
      console.log('mysql db connected successfully');
+     return database;
   } catch (error) {
     console.log(error);
     process.exit(0)
   }
 }
 
-
+export const getConnection = () => database
