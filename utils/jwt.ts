eee9921449bd503df06403d118d40bdadca7f857
chigozie <ezechukwuchigozie@gmail.com>
2022-10-11 19:47:34 +0100

    added jwt encode and decode utils

---
+++ utils/jwt.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import jwt from "jsonwebtoken";
+
+const JWTKEY = process.env.JWTKEY;
+
+export const encode = (payload: object) => {
+  return jwt.sign(payload, JWTKEY as string);
+};
+export const decode = (payload: string) => {
+  return jwt.verify(payload, JWTKEY as string);
+};
