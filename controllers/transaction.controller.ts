8cf50ade798dc418bdee824503adb8ac6899000b
chigozie <ezechukwuchigozie@gmail.com>
2022-10-12 08:05:35 +0100

    added controllers for balance withdraw and fund

--- controllers/transaction.controller.ts
+++ controllers/transaction.controller.ts
@@ -11,12 +11,37 @@ export class TransactionController {
 			amount: joi.number().positive().required(),
 		});
 	}
+    
+    static get transferSchema() {
+		return joi.object().keys({
+			amount: joi.number().positive().required(),
+            reciever: joi.string().required(),
+		});
+	}
 
 
 	// eslint-disable-next-line no-unused-vars
 	static async fundWallet(req: IExpressRequest | Request, res: IExpressResponse | Response, next: NextFunction) {
         const user = (req as IExpressRequest).user;
 		const transaction = await TransactionService.fundWallet(req.body.amount as number, user as User);
+		(res.status(201) as IExpressResponse).data(transaction);
+	}
+
+	static async balance(req: IExpressRequest | Request, res: IExpressResponse | Response, next: NextFunction) {
+        const user = (req as IExpressRequest).user;
+		const balance = await TransactionService.balance(user as User);
+		(res.status(200) as IExpressResponse).data(balance);
+	}
+	
+    static async withdraw(req: IExpressRequest | Request, res: IExpressResponse | Response, next: NextFunction) {
+        const user = (req as IExpressRequest).user;
+		const transaction = await TransactionService.withdraw(req.body.amount as number, user as User);
+		(res.status(201) as IExpressResponse).data(transaction);
+	}
+
+    static async transfer(req: IExpressRequest | Request, res: IExpressResponse | Response, next: NextFunction) {
+        const user = (req as IExpressRequest).user;
+		const transaction = await TransactionService.transfer(req.body as Pick<Transaction, 'amount' | 'reciever'>, user as User);
 		(res.status(201) as IExpressResponse).data(transaction);
 	}
 };
