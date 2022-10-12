e5f0fc6c6d7fe94ce0590ef64b67c8ae543efbda
chigozie <ezechukwuchigozie@gmail.com>
2022-10-12 14:13:35 +0100

    updated timestamp default value on migrations

--- migrations/20221012130055_transaction.ts
+++ migrations/20221012130055_transaction.ts
@@ -10,7 +10,8 @@ export async function up(knex: Knex): Promise<void> {
         table.enum('trans_type', ['DEBIT', 'CREDIT']);
         table.float('amount');
         table.enum('trans_category', ["DEPOSIT", "WITHDRAWAL", "TRANSFER"]);
-        table.timestamps(true, true);
+        table.dateTime('created_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+        table.dateTime('updated_at').notNullable().defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
     })
 }
 
