110e21b25ea0a5924558efa902a053287d7e16a5
chigozie <ezechukwuchigozie@gmail.com>
2022-10-13 16:22:27 +0100

    connected knex to Objections in db file

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,6 @@
 import databaseConfig from '../knexfile'
 import Knex, { Knex as KnexType} from 'knex';
+import { Model } from 'objection';
 
 // database singleton
 const env = (process.env.NODE_ENV || 'development') as keyof typeof databaseConfig;
@@ -8,6 +9,7 @@ const database: KnexType<any, unknown[]> = Knex(databaseConfig[env]);
 export const connectDatabase =  async () => {
   try {
      await database.raw('SELECT 1');
+     Model.knex(database);
      console.log('mysql db connected successfully');
      return database;
   } catch (error) {
