c6ae77c52764d35259f10221319e89cec72640e2
chigozie <ezechukwuchigozie@gmail.com>
2022-10-13 16:14:32 +0100

    added check to ensure user exists before withdrawal and ensure users cant transfer funds to them selves

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -17,13 +17,15 @@ export default class TransactionService {
 
 	static async withdraw(amount: number, user: User ) {
 		const userExists = await UserModel.query().findOne({id: user.id});
-		if(!userExists) return Promise.reject('User does not exist, are you trying to be mischeivous?'); 
+		if(!userExists) return Promise.reject('User not found!'); 
         return TransactionModel.withdraw(amount, user)
 	}
 
 	static async transfer(transferDetails: Pick<Transaction, "amount" | "reciever">, user: User ) {
 		const userExists = await UserModel.query().findOne({id: transferDetails.reciever});
 		if(!userExists) return Promise.reject('that user is yet to join our platform, maybe send a signup link to them first?'); 
+
+		if(userExists.id === user.id) return Promise.reject(' sorry you cant transfer funds to yourself, wouldnt that be nice though!'); 
         return TransactionModel.transfer(transferDetails, user)
 	}
 
